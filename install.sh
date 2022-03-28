4a710121ea99a10d7a464e513e308a725c11fa5a
Piyush Kumar Baliyan <piyush.kumar@admitkard.com>
2022-03-28 22:44:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install ansible wget
+sudo apt install ansible wget -y
 wget https://github.com/admitkard/ansible-for-dev/archive/refs/heads/master.zip -O ansible-for-dev.zip
 unzip -o ansible-for-dev.zip
 cd ansible-for-dev-master
