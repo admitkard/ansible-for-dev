258b37d92eaa5210d95e44c6cc8825626a9bf933
Piyush Kumar <piyush.kumar@admitkard.com>
2022-03-28 21:54:40 +0530

    Fixes permission

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 sudo apt install ansible wget
 wget https://github.com/admitkard/ansible-for-dev/archive/refs/heads/master.zip -O ansible-for-dev.zip
-unzip ansible-for-dev.zip
+unzip -o ansible-for-dev.zip
 cd ansible-for-dev-master
+chmod +x ./run.sh
 ./run.sh
