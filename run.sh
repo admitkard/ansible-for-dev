42ec208487990c0f46fd141c02e7b6652d22702e
Piyush Kumar <piyush.kumar@admitkard.com>
2022-03-29 18:54:56 +0530

    Fixes chrome installtion, and current user

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 export CURRENT_USER=$USER
-ansible-playbook -c local -i inventory setup.yml -vvv --ask-become-pass --extra-vars "user=$CURRENT_USER"
+ansible-playbook -c local -i inventory setup.yml -vvv --ask-become-pass --extra-vars "@userVariables.yml"
