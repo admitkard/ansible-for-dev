36a1197edd56cb9195d24562b5b682f12d44c1e6
Piyush Kumar <piyush.kumar@admitkard.com>
2022-03-29 12:47:36 +0530

    Removes unused file

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 ansible-galaxy collection install community.general
 ansible-galaxy collection install ansible.posix
-ansible-playbook -c local -i inventory setup.yml -vvv --ask-become-pass
+export CURRENT_USER=$USER
+ansible-playbook -c local -i inventory setup.yml -vvv --ask-become-pass --extra-vars "user=$CURRENT_USER"
