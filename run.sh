707eb3ed9af04a2160494ab5c41a0a4c43646465
Piyush Kumar <piyush.kumar@admitkard.com>
2022-03-29 14:14:05 +0530

    Adds github config

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
-ansible-galaxy collection install community.general
-ansible-galaxy collection install ansible.posix
 export CURRENT_USER=$USER
 ansible-playbook -c local -i inventory setup.yml -vvv --ask-become-pass --extra-vars "user=$CURRENT_USER"
