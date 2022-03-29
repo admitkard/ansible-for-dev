3ff99752d5c54d87adbb6faaa4988be0316d3f8d
Piyush Kumar <piyush.kumar@admitkard.com>
2022-03-29 14:23:12 +0530

    Fixes ansible installation

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-sudo apt install ansible wget -y
-wget https://github.com/admitkard/ansible-for-dev/archive/refs/heads/master.zip -O ansible-for-dev.zip
+sudo apt install curl wget -y
+# install pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py
+rm -rf get-pip.py
+
+# install ansible
+sudo python -m pip install ansible
+
+# download ansible-repo
+wget https://codeload.github.com/admitkard/ansible-for-dev/zip/refs/heads/master -O ansible-for-dev.zip
 unzip -o ansible-for-dev.zip
+rm -rf ansible-for-dev.zip
 cd ansible-for-dev-master
 chmod +x ./run.sh
-rm -rf ../ansible-for-dev.zip
 ./run.sh
